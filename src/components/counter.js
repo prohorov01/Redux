a4796caf2b551dd80ae0339f42ec8a6c2b30b5c1
Alex <alexprohorov01@gmail.com>
2023-08-14 14:38:04 +0300

    update counter

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,37 +1,35 @@
 import { connect } from "react-redux";
 import { inc, dec, rnd } from "../actions";
+import { useSelector, useDispatch } from "react-redux";
+// import { bindActionCreators } from "redux";
 
-const Counter = ({ counter, inc, dec, rnd }) => {
+const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   return (
     <div className="jumbotron">
       <h1>{counter}</h1>
-      <button onClick={dec} className="btn btn-primary">
+      <button onClick={() => dispatch(dec())} className="btn btn-primary">
         DEC
       </button>
-      <button onClick={inc} className="btn btn-primary">
+      <button onClick={() => dispatch(inc())} className="btn btn-primary">
         INC
       </button>
-      <button onClick={rnd} className="btn btn-primary">
+      <button onClick={() => dispatch(rnd())} className="btn btn-primary">
         RND
       </button>
     </div>
   );
 };
-const mapStateToProps = (state) => {
-  return {
-    counter: state.value,
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     counter: state.value,
+//   };
+// };
 
-const MapDispanchToProps = (dispatch) => {
-  return {
-    inc: () => dispatch(inc()),
-    dec: () => dispatch(dec()),
-    rnd: () => {
-      const value = Math.floor(Math.random() * 10);
-      dispatch(rnd(value));
-    },
-  };
-};
+// const mapDispanchToProps = (dispatch) => {
+//   return bindActionCreators(actions, dispatch);
+// };
 
-export default connect(mapStateToProps, MapDispanchToProps)(Counter);
+// export default connect(mapStateToProps, actions)(Counter);
+export default Counter;
